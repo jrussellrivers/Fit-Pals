303d5ec1383123286394940786effeee1f45ebfb
Jordan Rivers <j.russell.rivers@gmail.com>
2020-07-10 15:34:06 -0400

    made events banner more readable

--- src/fillPage/cardDetails.js
+++ src/fillPage/cardDetails.js
@@ -1,6 +1,10 @@
 function cardDetails(element){
     let cardDetails = document.createElement('div')
-    cardDetails.innerHTML = element.assetDescriptions[0].description
+    if (element.assetDescriptions[0] != undefined){
+        cardDetails.innerHTML = element.assetDescriptions[0].description
+    } else{
+        cardDetails.innerHTML = 'No Description'
+    }
     cardDetails.setAttribute('class','hidden')
 
     return cardDetails
